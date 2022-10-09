6894ab0ba6524a9d6d3623f160138c5b29060c09
DmytroStepaniukGH <dimastepaniukk@gmail.com>
2022-10-10 01:24:44 +0300

    HW2 task3 fixed

--- HT_2/HW2_task_3.py
+++ HT_2/HW2_task_3.py
@@ -5,8 +5,4 @@
 if __name__ == "__main__":
     number = int(input('Please, input a number: '))
 
-    sum_of_numbers = 0
-    for i in range(1, number+1):
-        sum_of_numbers += i
-
-    print(f'Sum of first positive integers up to and including {number} = {sum_of_numbers}')
+    print(f'Sum of first positive integers up to and including {number} = {sum(range(1, number+1))}')
