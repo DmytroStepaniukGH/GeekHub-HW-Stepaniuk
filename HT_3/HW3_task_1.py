9afec377dd2de9955765c163b78804f8586a981b
DmytroStepaniukGH <dimastepaniukk@gmail.com>
2022-10-13 13:21:34 +0300

    HW_3 task_1 fixed

--- HT_3/HW3_task_1.py
+++ HT_3/HW3_task_1.py
@@ -8,5 +8,12 @@ if __name__ == "__main__":
     value = input('Input value: ')
     list_of_tuples = [(), (5, 6, 8, -4, 7, 33), (98, 6, 32)]
 
-    list_of_tuples = [item[:-1]+(value,) for item in list_of_tuples if item]
-    print(f'Result: {list_of_tuples}')
+    result_list = []
+    for item in list_of_tuples:
+        if not item:
+            result_list.append(item)
+        else:
+            result_list.append(item[:-1]+(value,))
+
+    print(f'Result: {result_list}')
+
