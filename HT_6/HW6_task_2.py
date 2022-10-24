2ccfdb351089ec1a4500164c6d63bf4c16e59ce1
DmytroStepaniukGH <dimastepaniukk@gmail.com>
2022-10-24 10:41:26 +0300

    HW_6 task_2 fix

--- HT_6/HW6_task_2.py
+++ HT_6/HW6_task_2.py
@@ -16,28 +16,37 @@ class ValidateException(Exception):
 
 
 def validate(login, password):
-    pattern_login = re.compile('^[A-Za-z]{3,50}$')
-    pattern_password = re.compile(r'^(?=.*[0-9].*)(?=.*[A-Za-z].*)'
-                                  r'(?=.*[!@#$%].*)[0-9a-zA-Z!@#$%]{8,}$')
-    check_login = False
-    check_password = False
+    general_pattern = re.compile('^[0-9a-zA-Z!@#$%]+$')
+    char = re.compile('[A-Za-z]')
+    number = re.compile('[0-9]')
+    spec_char = re.compile('[!@#$%]')
+    errors = ''
 
-    if pattern_login.search(login):
-        check_login = True
-    if pattern_password.search(password):
-        check_password = True
-
-    if check_login and check_password:
-        return f'{"All ok"}'
-    elif check_login and not check_password:
-        return ValidateException('The password does not meet the '
-                                 'requirements')
-    elif not check_login and check_password:
-        return ValidateException('The login does not meet the '
-                                 'requirements')
+    try:
+        re.search(general_pattern, login).group()
+        re.search(general_pattern, password).group()
+    except AttributeError:
+        return ValidateException('Entered characters cannot be used as '
+                                 'login/password characters.\nNote: valid '
+                                 'characters for the login are letters of '
+                                 'the Latin alphabet.\nFor the password - '
+                                 'letters of the Latin alphabet, numbers '
+                                 'and special characters !@#$%.')
     else:
-        return ValidateException('The login and password do not meet the '
-                                 'requirements')
+        if not char.search(login):
+            errors += 'The login must contain only Latin letters.\n'
+        if len(login) not in range(3, 51):
+            errors += 'The length of the login should be from 3 to 50.\n'
+        if not char.search(password):
+            errors += 'The password must contain Latin letters.\n'
+        if not number.search(password):
+            errors += 'The password must contain numbers.\n'
+        if not spec_char.search(password):
+            errors += 'The password must contain special symbols (!@#$%).\n'
+        if len(password) < 8:
+            errors += 'Password must be 8 or more characters long\n'
+
+        return 'OK' if len(errors) == 0 else ValidateException(f'{errors}')
 
 
 if __name__ == "__main__":
